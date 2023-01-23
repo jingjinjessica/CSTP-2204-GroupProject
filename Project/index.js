ec6d837650bdae04e9dbef1abf0fc58b4d308e9c
Gina Sun <ginasun28@gmail.com>
2023-01-23 11:20:22 -0800

    Add login, register ejs file and css

--- Project/index.js
+++ Project/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
+const path = require('path')
 
 require("dotenv").config();
 
 app.use(express.json());
-
-app.use(express.static("./client/public"));
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname + '/client/public')))
+// app.use(express.static("/client/public"));
 app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
 
+app.get('', (req, res) => {
+  res.render('index')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// run npm start
