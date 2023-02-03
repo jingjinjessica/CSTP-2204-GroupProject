cb08cb9daca300933f13c33e72186aef209b556d
Yifei Chen <melodychen823@gmail.com>
2023-02-03 15:17:38 +0800

    create cloudinary middleware multer and image

--- Project/app.js
+++ Project/app.js
@@ -2,31 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 const userRoutes = require("./server/routes/users");
 const profileRoutes = require("./server/routes/profiles");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
 // const multer = require("../library/multer");
 
 // const cloudinary = require("../library/cloudinary");
-require('dotenv').config()
-
+require("dotenv").config();
 
 //cookie
 app.use(cookieParser());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname + '/client/public')))
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(
-  fileUpload()
-);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + "/client/public")));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(fileUpload());
 app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
@@ -38,38 +35,33 @@ mongoose.connect(process.env.MONGO_URL, (error) => {
   }
 });
 
+app.get("/index", (req, res) => {
+  res.render("pages/index", { title: "Home" });
+});
 
-app.get('/index', (req, res) => {
-  res.render('pages/index', { 'title': 'Home', })
-})
+app.get("/login", (req, res) => {
+  res.render("pages/login", { title: "Login" });
+});
 
-app.get('/login', (req, res) => {
-  res.render('pages/login', { 'title': 'Login' })
-})
+app.get("/register", (req, res) => {
+  res.render("pages/register", { title: "Register" });
+});
 
-app.get('/register', (req, res) => {
-  res.render('pages/register', { 'title': 'Register' })
-})
+app.get("/createpetowner", (req, res) => {
+  res.render("pages/createPetOwner");
+});
 
+app.get("/createPetSitter", (req, res) => {
+  res.render("pages/createPetSitter");
+});
 
+app.get("/petOwnerPost", (req, res) => {
+  res.render("pages/petOwnerPost");
+});
 
-// app.get('/createPetOwner', (req, res) => {
-//   res.render('pages/createPetOwner')
-// })
-
-// app.get('/createPetSitter', (req, res) => {
-//   res.render('pages/createPetSitter')
-// })
-
-// app.get('/petOwnerPost', (req, res) => {
-//   res.render('pages/petOwnerPost')
-// })
-
-// app.get('/petSitterPost', (req, res) => {
-//   res.render('pages/sitterPost')
-// })
-
-
+app.get("/petSitterPost", (req, res) => {
+  res.render("pages/sitterPost");
+});
 
 function userLogger(req, res, next) {
   console.log("Loading User requests....");
@@ -81,8 +73,7 @@ function postLogger(req, res, next) {
   next();
 }
 
-
-function profileLogger(req,res,next) {
+function profileLogger(req, res, next) {
   console.log("Loading Profile Post requests....");
   next();
 }
@@ -95,10 +86,6 @@ app.use("/api/v1/users", userLogger, userRoutes);
 //app.use("/api/v1/posts", postLogger, postRoute);
 
 app.use("/profile", profileRoutes);
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
