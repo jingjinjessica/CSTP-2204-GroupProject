9f7c0e74f0666435b831bde758c6653c24023fdd
Yifei Chen <melodychen823@gmail.com>
2023-02-18 10:33:19 +0800

    update the id url

--- Project/app.js
+++ Project/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 const mongoose = require("mongoose");
 const path = require("path");
 const userRoutes = require("./server/routes/users");
--- Project/server/controller/list.js
+++ Project/server/controller/list.js
@@ -3,27 +3,23 @@ const jwt = require("jsonwebtoken"); // This Library will help us give and verif
 const Profile = require("../model/Profile");
 const PetPost = require("../model/PetPost");
 
-
-const getPost = async (req,res) => {
-    //connect 2 tables profiles and petpost
-    const result = await PetPost.aggregate([
+const getPost = async (req, res) => {
+  //connect 2 tables profiles and petpost
+  const result = await PetPost.aggregate([
     {
-        $lookup:{
-            from: "profiles",
-            localField:"userID",
-            foreignField:"userID",
-            as:"profile"
-        }
-    }
-]);
-    //console.info(result[8].profile[0]);
-    res.render("pages/list",{result:result})
-}
-
-
+      $lookup: {
+        from: "profiles",
+        localField: "userID",
+        foreignField: "userID",
+        as: "profile",
+      },
+    },
+  ]);
+  console.log("this is result", result);
+  //console.info(result[8].profile[0]);
+  res.render("pages/list", { result: result });
+};
 
 module.exports = {
-    getPost
-  };
-  
-
+  getPost,
+};
