839709bc193bd3974b3ee5e207b6c04a9c38d992
Gina Sun <ginasun28@gmail.com>
2023-03-17 14:42:35 -0700

    black box tesing for gina

--- Project/server/routes/lists.js
+++ Project/server/routes/lists.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getPetPost} = require("../controller/petPostList");
+// const { getPetPost} = require("../controller/petPostList");
 const { getSitterPost} = require("../controller/sitterPostList");
+
+// testing
+const { getPetPost} = require("../controller/filterTest");
 
 router.get("/listpetpost",getPetPost);
 router.post("/listpetpost",getPetPost);
