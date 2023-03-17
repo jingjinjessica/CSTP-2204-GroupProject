708a5e99f08afd9e61f19b4bd49a625888b50dba
Jing Jin <jingjin496@gmail.com>
2023-03-17 16:10:28 -0700

    blackbox test for owner profile create and update

--- Project/server/routes/profiles.js
+++ Project/server/routes/profiles.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const {createPetOwner, createPetSitter, getProfile,createOwnerPost,createSitterPost} = require("../controller/profile");
-
+//const {createPetOwner, createPetSitter, getProfile,createOwnerPost,createSitterPost} = require("../controller/profile");
+const {createPetOwner, getProfile,createOwnerPost} = require("../controller/test-owner-profile");
 
 router.get("/getProfile",getProfile);
 router.get("/createPetOwner",createPetOwner);
-router.get("/createPetSitter",createPetSitter);
+//router.get("/createPetSitter",createPetSitter);
 
 
 router.post("/create",createOwnerPost);
-router.post("/createsitter",createSitterPost);
+//router.post("/createsitter",createSitterPost);
 
 
 module.exports = router;
