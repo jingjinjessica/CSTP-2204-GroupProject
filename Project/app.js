f67897a16b0639711909039a300b0891b12e461e
Nicole308 <91211275+Nicole308@users.noreply.github.com>
2023-03-27 11:36:36 -0700

    Created the chat page

--- Project/app.js
+++ Project/app.js
@@ -12,6 +12,41 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
+// SOCKET IO TEST FOR CHAT 
+var http = require("http").Server(app)
+var io = require('socket.io')(http)
+
+// Testing for socket.io
+io.on('connection', (socket) => {
+  console.log("Socket io connected");
+
+  socket.on("msg", (data) => {
+      io.sockets.emit('newMsg', data);
+  });
+
+  socket.on('disconnect', function(){
+    console.log("User is disconnected")
+});
+});
+
+// FAKE DATA FOR CHAT TESTING
+const peopleList = [{
+  "id": 1,
+  "avatar_img": "/image/cat-dog-cuddle.jpg",
+  "full_name": "Torey Groven",
+  "chat_msg": ["Hello..my name is Torey. Nice to meet you", "test"],
+}, {
+  "id": 2,
+  "avatar_img": "/image/black-cat.jpg",
+  "full_name": "Sheridan Freebury",
+  "chat_msg": ["I love youuuu and I also hate you"]
+}, {
+  "id": 3,
+  "avatar_img": "/image/three-pets-laying.jpg",
+  "full_name": "Ann Noweak",
+  "chat_msg": ["I wanna eat ramen for some reason"]
+}]
+
 // const multer = require("../library/multer");
 
 // const cloudinary = require("../library/cloudinary");
@@ -45,11 +80,11 @@ app.get("/login", (req, res) => {
   res.render("pages/login", { title: "Login" });
 });
 
-// app.get("/register", (req, res) => {
-//   res.render("pages/register", { title: "Register" });
-// });
+app.get("/register", (req, res) => {
+  res.render("pages/register", { title: "Register" });
+});
 
-app.get("/createPetOwner", (req, res) => {
+app.get("/createpetowner", (req, res) => {
   res.render("pages/createPetOwner");
 });
 
@@ -63,6 +98,11 @@ app.get("/petOwnerPost", (req, res) => {
 
 app.get("/petSitterPost", (req, res) => {
   res.render("pages/sitterPost");
+});
+const renderedData = encodeURIComponent(JSON.stringify(peopleList));
+app.get("/chat", (req, res) => {
+  res.render("pages/chat", {peopleList: peopleList, renderedData})
+  
 });
 
 function userLogger(req, res, next) {
@@ -90,7 +130,7 @@ app.use("/api/v1/ownerposts", postLogger, ownerRoutes);
 
 app.use("/profile", profileRoutes);
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 // run npm start
