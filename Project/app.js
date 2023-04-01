a206d4329918c30e9de0b251509d4841248d4929
Yifei Chen <melodychen823@gmail.com>
2023-03-31 18:48:08 -0700

    delete fake data

--- Project/app.js
+++ Project/app.js
@@ -55,26 +55,6 @@ app.get("/register", (req, res) => {
 });
 
 // FAKE DATA FOR CHAT TESTING
-const peopleList = [
-  {
-    id: 1,
-    avatar_img: "/image/cat-dog-cuddle.jpg",
-    full_name: "Torey Groven",
-    chat_msg: ["Hello..my name is Torey. Nice to meet you", "test"],
-  },
-  {
-    id: 2,
-    avatar_img: "/image/black-cat.jpg",
-    full_name: "Sheridan Freebury",
-    chat_msg: ["I love youuuu and I also hate you"],
-  },
-  {
-    id: 3,
-    avatar_img: "/image/three-pets-laying.jpg",
-    full_name: "Ann Noweak",
-    chat_msg: ["I wanna eat ramen for some reason"],
-  },
-];
 
 // async function getCurrProfile() {
 //   const response = await fetch("/profile/getCurrUserProfile", {
